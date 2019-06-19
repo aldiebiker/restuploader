a44ab42d5e61e1c627ea8a424549652185315c48
Senthil <senthil>
2019-06-18 22:16:57 -0400

    Get rid of deprecated apis.

--- src/test/java/com/restuploader/spring/RestuploaderControllerTest.java
+++ src/test/java/com/restuploader/spring/RestuploaderControllerTest.java
@@ -9,9 +9,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -22,34 +28,33 @@ public class RestuploaderControllerTest {
 
 	@MockBean
 	private FileUploaderService fileUploaderService;
-	
+
 	MockMultipartFile multipartFile;
-	
+
 	@Before
 	public void setUp() {
-		multipartFile = new MockMultipartFile("fileName", "test.txt",
-                "text/plain", "Spring Framework".getBytes());
+		multipartFile = new MockMultipartFile("fileName", "test.txt", "text/plain", "Spring Framework".getBytes());
 	}
-	
+
 	@Test
 	public void returnSuccessOnHappyPath() throws Exception {
-        this.mvc.perform(fileUpload("/files/").file(multipartFile))
-                .andExpect(status().is2xxSuccessful());
+
+		this.mvc.perform(MockMvcRequestBuilders.multipart("/files/").file(multipartFile)).andExpect(status().is(200));
 
 	}
-	
 
 	@Test
 	public void returnFailureOnInvalidPath() throws Exception {
-        this.mvc.perform(fileUpload("/files/upload").file(multipartFile))
-                .andExpect(status().is4xxClientError());
-
+		this.mvc.perform(MockMvcRequestBuilders.multipart("/files/upload").file(multipartFile))
+				.andExpect(status().is(404));
 	}
-	
+
 	@Test
 	public void returnFailureOnInvalidParamName() throws Exception {
-        this.mvc.perform(fileUpload("/files/upload").file(multipartFile))
-                .andExpect(status().is4xxClientError());
-
+		MockMultipartFile multipartWrongFileName = new MockMultipartFile("file", "test.txt", "text/plain",
+				"Spring Framework".getBytes());
+		this.mvc.perform(MockMvcRequestBuilders.multipart("/files/").file(multipartWrongFileName))
+				.andExpect(status().is(400));
 	}
+
 }
